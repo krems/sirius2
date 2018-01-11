28f3a2ccb7a223f62a369464f2a3db4a95b6c383
Valerii Ovchinnikov <valerii.ovchinnikov@revolut.com>
2018-01-12 00:38:31 +0300

    decorated executor

--- src/main/java/com/sirius/excercise/PumpedExecutorService.java
+++ src/main/java/com/sirius/excercise/PumpedExecutorService.java
@@ -5,68 +5,70 @@ import java.util.List;
 import java.util.concurrent.*;
 
 public class PumpedExecutorService implements ExecutorService {
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    
     @Override
     public <T> Future<T> submit(final Callable<T> task) {
-        return null;
+        return executor.submit(task);
     }
     
     @Override
     public <T> Future<T> submit(final Runnable task, final T result) {
-        return null;
+        return executor.submit(task, result);
     }
     
     @Override
     public Future<?> submit(final Runnable task) {
-        return null;
+        return executor.submit(task);
     }
     
     @Override
     public void execute(final Runnable command) {
-    
+        executor.execute(command);
     }
     
     @Override
     public void shutdown() {
-    
+        executor.shutdown();
     }
     
     @Override
     public List<Runnable> shutdownNow() {
-        return null;
+        return executor.shutdownNow();
     }
     
     @Override
     public boolean isShutdown() {
-        return false;
+        return executor.isShutdown();
     }
     
     @Override
     public boolean isTerminated() {
-        return false;
+        return executor.isTerminated();
     }
     
     @Override
     public boolean awaitTermination(final long timeout, final TimeUnit unit) throws InterruptedException {
-        return false;
+        return executor.awaitTermination(timeout, unit);
     }
     
     @Override
     public <T> List<Future<T>> invokeAll(final Collection<? extends Callable<T>> tasks) throws InterruptedException {
-        return null;
+        return executor.invokeAll(tasks);
     }
     
     @Override
     public <T> List<Future<T>> invokeAll(final Collection<? extends Callable<T>> tasks, final long timeout, final TimeUnit unit) throws InterruptedException {
-        return null;
+        return executor.invokeAll(tasks, timeout, unit);
     }
     
     @Override
     public <T> T invokeAny(final Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
-        return null;
+        return executor.invokeAny(tasks);
     }
     
     @Override
     public <T> T invokeAny(final Collection<? extends Callable<T>> tasks, final long timeout, final TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        return null;
+        return executor.invokeAny(tasks, timeout, unit);
     }
 }
